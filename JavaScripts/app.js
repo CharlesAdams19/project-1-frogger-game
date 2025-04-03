1514e0e3ce6dbdf2835e7b4e14f00b53127c69f5
Charles Adams 19 <adamscra19@gmail.com>
2025-04-03 16:16:17 +0100

    code

--- JavaScripts/app.js
+++ JavaScripts/app.js
@@ -8,7 +8,7 @@ const totalCellCount = gridColums * gridRows
 
 const gridCells = []
 const startZoneRow = gridRows - 1
-const startPosition = startZoneRow * gridColums + Math.floor(gridColums/2)
+const startPosition = startZoneRow * gridColums + Math.floor(gridColums / 2)
 
 
 
@@ -18,54 +18,47 @@ const startSection = document.querySelector('#Start')
 const playButton = document.querySelector('#play-button')
 const gridWrapper = document.querySelector('.grid-wrapper')
 const gridContainer = document.querySelector('.grid')
-let currentPosition = startPosition
-let fatigue = 0 
 const livesDisplay = document.querySelector('#lives')
 const endSection = document.querySelector('#End')
 const playAgainButton = document.querySelector('#play-again-button')
+const scoreDisplay = document.querySelector('#score-display')
+
+
+let currentPosition = startPosition
+let fatigue = 0
+let resetPause = false
+let score = 0 
+
 
 // ## start safe and end zone ##
 
-
-
-
-
-// obstacles:  store the obstacles 
-
+// obstacles 
 const obstacles = []
-const obstacleRows = [6, 7, 8]
+const obstacleRows = [1, 2, 5, 6, 8, 18]
 
-//  gridCells[index].class.add('obstacle')
-
-
-
-// function 
 
 // ## Grid ##
 
 function generateGameScreen() {
-    const gridRows = 20
-    const gridColums = 20
-    const totalCellCount = gridColums * gridRows
+    const gridTemplateRows = []
+    for (let row = 0; row < gridRows; row++) {
+      if (row === 0 || row === 10 || row === gridRows - 1) {
+        gridTemplateRows.push('3fr')
+      } else {
+        gridTemplateRows.push('1fr')
+      }
+    }
+    gridContainer.style.gridTemplateRows = gridTemplateRows.join(' ')
+
     for (let idx = 0; idx < totalCellCount; idx++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
-        cell.style.width = `${100 / gridColums}%`
-        cell.style.height = `${100 / gridRows}%`
-        cell.innerText = idx
-
-       
-
-// add start, safe and end zones 
+        
 
         const row = Math.floor(idx / gridColums)
-
-// flexible zones 
-
-        const isEndZone = row === 0 
-        const isSafeZone = row % 4 === 0 
-        const isStartZone = row === gridRows - 1
-
+        const isEndZone = row === 0
+        const isSafeZone = row === 10
+        const isStartZone = row === startZoneRow
 
         if (isEndZone) {
             cell.classList.add('end-zone')
@@ -74,117 +67,112 @@ function generateGameScreen() {
         } else if (isStartZone) {
             cell.classList.add('start-zone')
         }
-
-        // const isStartZoneRow = gridRows -1
-        // const startPosition = isStartZoneRow * gridColums + Math.floor(gridColums/2)
-
         gridContainer.appendChild(cell)
         gridCells.push(cell)
 
     }
-    addNessie(startPosition)
+    currentPosition =startPosition
+    addNessie(currentPosition)
 }
-
-//  ## player counter movement ## // check for obstacles within the movement
 
 
 function addNessie(position) {
+   if (gridCells[position]){
     gridCells[position].classList.add('nessie')
-
+}
 }
 
 function removeNessie(position) {
+    if (gridCells[position]) {
     gridCells[position].classList.remove('nessie')
+}
 }
 
 function moveNessie(event) {
     const pressedKey = event.code
 
-    if (pressedKey === 'ArrowUp' && gridColums <= currentPosition) {
+    if (pressedKey === 'ArrowUp' && currentPosition>= gridColums) {
         removeNessie(currentPosition)
-        addNessie(currentPosition -= gridColums)
-        if (gridCells[currentPosition].classList.contains('obstacle')) {
+        currentPosition -= gridColums
+        addNessie(currentPosition)
+        if (gridCells[currentPosition].classList.contains('obstacle')) 
             reset()
-        }
-        if (gridCells[currentPosition].classList.contains('end-zone')) {
+        if (gridCells[currentPosition].classList.contains('end-zone')) 
             win()
+
+        }
+        else if (pressedKey === 'ArrowDown' && currentPosition + gridColums < totalCellCount) {
+            removeNessie(currentPosition)
+            currentPosition += gridColums
+            addNessie(currentPosition)
+        }
+        else if (pressedKey === 'ArrowLeft' && currentPosition % gridColums !== 0) {
+            removeNessie(currentPosition)
+            currentPosition--
+            addNessie(currentPosition)
+        }
+        else if (pressedKey === 'ArrowRight' && currentPosition % gridColums !== gridColums - 1) {
+            removeNessie(currentPosition)
+            currentPosition++
+            addNessie(currentPosition)
+        }
     }
-    }
-    else if (pressedKey === 'ArrowDown' && currentPosition + gridColums <= totalCellCount - 1) {
+
+
+
+    function reset() {
+        if (resetPause) return
+        resetPause = true
+      
         removeNessie(currentPosition)
-        addNessie(currentPosition += gridColums)
-    }
-    else if (pressedKey === 'ArrowLeft' && currentPosition % gridColums !== 0) {
+        currentPosition = startPosition
+        fatigue++
+      
+        if (fatigue >= 3) {
+          gameOver()
+        } else {
+          addNessie(currentPosition)
+          updateFatigueDisplay()
+        }
+      
+        setTimeout(() => {
+          resetPause = false
+        }, 2000)
+      }
+      
+      function gameOver() {
+        gridWrapper.classList.add('hide')
+        endSection.classList.remove('hide')
+        livesDisplay.textContent = 'Fatigue: 🛌'
+      }
+    
+      function win() {
+        updateScore()
         removeNessie(currentPosition)
-        currentPosition--
+        currentPosition = startPosition
         addNessie(currentPosition)
-    }
-    else if (pressedKey === 'ArrowRight' && currentPosition % gridColums !== gridColums - 1) {
-        removeNessie(currentPosition)
-        currentPosition++
-        addNessie(currentPosition)
-    }
-}
-
-
-
-function reset() {
-    removeNessie(currentPosition)
-    currentPosition = startPosition
-    fatigue++
-
-    if (fatigue >= 3) {
-        gameOver()
-    }else {
-        addNessie(currentPosition)
-        updateFatigueDisplay()
+    
     }
 
-}
 
-function win () {
-console.log ('Nessie reached the end zone!')
+      function updateScore() {
+        score += 100
+        scoreDisplay.textContent = `Score: ${score}`
+      }
 
+      // ## lives display = fatigue 
 
-}
+    function updateFatigueDisplay() {
 
+        const fatigueLevels = ['😌', '😪', '💤']
+        const display = fatigue < 3 ? fatigueLevels[fatigue] : '🛌'
+        livesDisplay.textContent = `Fatigue: ${display}`
 
-// ## play game ##
+    }
 
-function playGame() {
+ // ##obstacles##
 
-    startSection.classList.add('hide')
-    gridWrapper.classList.remove('hide')
-    generateGameScreen()
-
-    createObstacle(6)
-    setInterval(moveObstacles, 500)
-    updateFatigueDisplay()
-
-
-}
-
-function gameOver(){
-
-    gridWrapper.classList.add('hide')
-    endSection.classList.remove('hide')
-    livesDisplay.textContent = 'Fatigue: 🛌'
-
-
-}
-// ## lives display = fatigue 
-
-function updateFatigueDisplay () {
-
-    const fatigueLevels = ['😌', '😪', '💤']
-    const display =fatigue <3 ? fatigueLevels[fatigue] :'🛌' 
-    livesDisplay.textContent = `Fatigue: ${display}`
-
-}
-
-// ##obstacles##
-
-function createObstacle(rowIndex, length = 2) {
+ function createObstacle(rowIndex, length = 2) {
     const startCol = Math.floor(Math.random() * (gridColums - length))
     const positionIndexes = []
 
@@ -220,28 +208,86 @@ function moveObstacles() {
                 return isLeftEdge ? index + (gridColums - 1) : index - 1
             }
         })
+        //  check for collision  with nessie 
+        if (obstacle.positions.includes(currentPosition)) 
+            reset()
+        
+        // add obstacle classes 
 
         obstacle.positions.forEach(index => {
             gridCells[index].classList.add('obstacle')
-          })
         })
-      }
+    })
+}
 
 
 
 
-// Events 
+
+
+
+
+
+    // ## play game ##
+
+    function playGame() {
+        startSection.classList.add('hide')
+        gridWrapper.classList.remove('hide')
+        generateGameScreen()
+        updateFatigueDisplay()
+        obstacleRows.forEach(row => {
+            for (let i = 0; i < 4; i++) {
+                createObstacle(row, 2)
+            }
+
+        }
+        )
+       
+        setInterval(moveObstacles, 1000)
+    }
+
+   
+
+   
+
+
+    // Events 
 
     playButton.addEventListener('click', playGame)
     document.addEventListener('keydown', moveNessie)
-    playAgainButton.addEventListener('click',() => {
+    playAgainButton.addEventListener('click', () => {
         location.reload()
 
     })
 
 
 
+// code graveyard 
 
 
-// elements 
 
+// const gridRows = 20
+// const gridColums = 20
+// const totalCellCount = gridColums * gridRows
+// const isStartZone =row === startZoneRow
+
+
+
+
+// add start, safe and end zones 
+
+
+
+// flexible zones 
+
+
+
+// const isStartZoneRow = gridRows -1
+// const startPosition = isStartZoneRow * gridColums + Math.floor(gridColums/2)
+
+
+//  ## player counter movement ## // check for obstacles within the movement
+
+// cell.style.width = `${100 / gridColums}%`
+        // cell.style.height = `${100 / gridRows}%`
+        // cell.innerText = idx
